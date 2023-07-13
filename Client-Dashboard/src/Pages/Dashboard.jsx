c86f71b08d4a4d7d335dfe9f2a3cc82c44b00f32
Shincy <shincyshnz@gmail.com>
2023-07-13 12:26:07 +0530

    loader added in dashboard

--- Client-Dashboard/src/Pages/Dashboard.jsx
+++ Client-Dashboard/src/Pages/Dashboard.jsx
@@ -1,23 +1,52 @@
 import React, { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import axios from "axios";
+import { useError } from "../context/ErrorContext";
 
 const Dashboard = () => {
+  const { errorObj, handleErrorObj, deleteErrorObj } = useError();
+
+  const [isLoading, setIsLoading] = useState(false);
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
+    setIsLoading(true);
     fetchMovies();
-  },[]);
+  }, []);
 
   const fetchMovies = async () => {
-    const response = await axios(import.meta.env.VITE_MOVIES_URL);
-    setMovieList(response.data);
+    try {
+      const response = await axios(import.meta.env.VITE_MOVIES_URL);
+      setMovieList(response.data);
+    } catch (error) {
+      deleteErrorObj("apiError");
+      handleErrorObj(
+        "apiError",
+        `${error.message} : Error fetching movie details`
+      );
+    }finally{
+      setIsLoading(false);
+    }
   };
 
   return (
     <div className="px-10 py-5 xl:py-20 xl:px-48 grid grid-cols-1 lg:grid-cols-2 gap-x-16 gap-y-4">
-      {movieList.map(movie=>{
-        return <MovieCard key={movie._id} movie={movie}/>
+      {isLoading ? (
+        <div
+          className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      ) : (
+        movieList.map((movie) => {
+          return <MovieCard key={movie._id} movie={movie} />;
+        })
+      )}
+      {errorObj?.map((err, index) => {
+        return err.apiError && <Error errorKey="apiError" key={index} />;
       })}
     </div>
   );
--- Client-Dashboard/src/components/RatingStars.jsx
+++ Client-Dashboard/src/components/RatingStars.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const RatingStars = ({ rating }) => {
-  const renderStar = (starColor) => {
+  const renderStar = (starColor,index) => {
     const htmlStar = (
       <svg
+        key={index}
         aria-hidden="true"
         className={`w-5 h-5 ${starColor}`}
         fill="currentColor"
@@ -12,7 +13,7 @@ const RatingStars = ({ rating }) => {
       >
         {/* <title>First star</title> */}
         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-      </svg>
+      </svg>      
     );
     return htmlStar;
   };
@@ -22,12 +23,12 @@ const RatingStars = ({ rating }) => {
       <div className="flex items-center">
         {[...Array(5)].map((e, i) => {
           return rating >= i + 1
-            ? renderStar("text-yellow-400")
-            : renderStar("text-gray-500");
+            ? renderStar("text-yellow-400",i)
+            : renderStar("text-gray-500",i);
         })}
-        {/* <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+        <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
           {rating} / 5
-        </p> */}
+      </p>
       </div>
     </div>
   );
